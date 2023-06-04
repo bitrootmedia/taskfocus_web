1854a0e112c14805f47fad9528c7fe4728553051
Samvel <samo97danielyan@gmail.com>
2023-06-04 12:07:21 +0400

    fix sidebar logo showing

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -25,8 +25,8 @@
             class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
             to="/"
         >
-          <img :src="config.LOGO_URL" alt="thumbnail" v-if="closePanel">
-          <span v-else>{{config.PROJECT_NAME}}</span>
+          <img v-if="config.LOGO_URL && closePanel" :src="config.LOGO_URL" alt="thumbnail">
+          <span v-else-if="!closePanel">{{config.PROJECT_NAME}}</span>
         </router-link>
 
         <button
