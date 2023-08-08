b52ec60e4b44c0a44d55f21f011ed522fba1a22d
Samvel <samo97danielyan@gmail.com>
2023-08-08 14:16:12 +0400

    update users page

--- src/views/dashboard/UsersTasks.vue
+++ src/views/dashboard/UsersTasks.vue
@@ -1,58 +1,79 @@
 <template>
   <div class="main-container">
+    <h2 class="font-bold text-xl block text-blueGray-700 mb-4">Users</h2>
 
-    <Loader v-if="loading"/>
+      <DataTable :headers="headers">
+        <template v-slot:tableBody>
+          <tr v-if="loading">
+            <td :colspan="headers.length">
+              <div class="flex justify-center py-1 text-blueGray-500 font-medium">
+                <Loader/>
+              </div>
+            </td>
+          </tr>
 
-    <template v-else>
-      <div class="content grid grid-cols-1 sm:grid-cols-2 gap-6">
-        <div v-for="(userTask,index) in usersTasks"
-             :key="index"
-             class="border-2 border-blueGray-300 rounded-[4px] px-4 pt-4">
-          <div class="text-center mb-4">
-            <h4 class="flex justify-center items-baseline text-xl font-bold inline-flex text-blueGray-800 cursor-pointer"
-                @click="toLink(`/dashboard/users-tasks/${Object.keys(userTask)[0]}`)">
-              {{ userTask[Object.keys(userTask)[0]].user.first_name }} {{
-                userTask[Object.keys(userTask)[0]].user.last_name
-              }}
-              <span class="text-sm ml-[2px] text-blueGray-500">({{userTask[Object.keys(userTask)[0]].user.username}})</span>
-            </h4>
-          </div>
+          <template v-else>
+            <tr v-if="!users.length">
+              <td :colspan="headers.length">
+                <p class="flex text-center px-4 justify-center py-8 text-blueGray-500 font-medium">
+                  No data found
+                </p>
+              </td>
+            </tr>
 
-          <p class="text-blueGray-600 font-semibold mb-3" v-if="userTask[Object.keys(userTask)[0]].currentTask?.id">
-            Currently working on:
-            <router-link :to="`/dashboard/task/${userTask[Object.keys(userTask)[0]].currentTask.id}`"
-                         class="underline text-blue-500">{{ userTask[Object.keys(userTask)[0]].currentTask.title }}
-            </router-link>
-          </p>
-        </div>
-      </div>
-    </template>
+           <tbody>
+            <tr v-for="element in users" :key="element.id">
+             <td class="border-t-0 px-3 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+               <span class="cursor-pointer" @click="toLink(`/dashboard/users-tasks/${element.id}`)">{{ element.first_name }} {{ element.last_name }}</span>
+             </td>
+             <td class="border-t-0 px-3 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+               <span class="cursor-pointer" @click="toLink(`/dashboard/users-tasks/${element.id}`)">{{ element.username }}</span>
+             </td>
+           </tr>
+           </tbody>
+          </template>
+        </template>
+      </DataTable>
+
+      <Pagination
+          v-if="paginate.pagination.value.total > 1 && !loading"
+          :pagination="paginate.pagination.value"
+          v-model:query="paginate.query.value"
+      />
+
   </div>
 </template>
 
 <script setup>
 import Loader from "./../../components/Loader/Loader.vue"
-import {useUsersTasksStore} from "../../store/users-tasks";
 import {computed, ref} from "vue";
 import {useToast} from "vue-toastification";
 import {useRouter} from "vue-router";
 import {catchErrors} from "../../utils";
 import {useUserStore} from "../../store/user";
 import {useCookies} from "vue3-cookies";
+import DataTable from '../../components/Table/DataTable.vue'
+import Pagination from '../../components/Pagination/Pagination.vue'
+import {usePaginate} from "../../composables/usePaginate";
 
-const usersTasksStore = useUsersTasksStore()
 const userStore = useUserStore()
 const {cookies} = useCookies();
 const toast = useToast()
 const router = useRouter()
 
-
 //State
 const loading = ref(false)
-const usersTasks = ref([])
 const users = ref([])
 const userConfig = ref(null)
 
+
+// Computed
+const headers = computed(() => {
+  return [
+    {id: 1, label: 'Full Name', sorting: false},
+    {id: 2, label: 'Username', sorting: false},
+  ]
+})
 
 // Methods
 const toLink = (link) => {
@@ -62,68 +83,20 @@ const toLink = (link) => {
 const fetchUsers = async () => {
   try {
     loading.value = true
-    const resp = await userStore.fetchUsersPage()
-    const users = resp.data.results
-
-    const filterUsers = users.filter((item)=>userConfig.value.limit_queue_view.includes(item.username))
-    usersTasks.value = []
-
-    await filterUsers.map(async (item) => {
-      await fetchUsersTasks(item)
-    })
+    const options = {
+      pagination: paginate.pagination.value,
+      query: paginate.query.value,
+    }
+    const resp = await userStore.fetchUsersPage(options)
+    users.value = resp.data.results
+    paginate.updatePagination(resp)
   } catch (e) {
     catchErrors(e)
-  }
-}
-
-const changeDrag = async (e, item) => {
-  try {
-    const newIndex = e.moved.newIndex
-    let aboveItemId = null
-    let belowItemId = null
-
-    if (newIndex !== 0) {
-      const findItem = item.data.find((item, index) => index === newIndex - 1)
-      aboveItemId = findItem?.id
-    }
-
-    if (newIndex !== item.data.length) {
-      const nextItem = item.data.find((item, index) => index === newIndex + 1)
-      belowItemId = nextItem?.id
-    }
-
-    const data = {
-      id: e.moved.element.id,
-      task_above_id: aboveItemId,
-      user_task_queue_id: belowItemId,
-    }
-    await usersTasksStore.updateOrder(data)
-  } catch (e) {
-    catchErrors(e)
-  }
-}
-
-const fetchUsersTasks = async (user) => {
-  try {
-    const resp = await usersTasksStore.fetchUsersTasks({id: user.id})
-    const temp = []
-
-    const obj = {
-      user: user,
-      data: resp.data.results,
-      currentTask: await usersTasksStore.fetchWorkingTask({id: user.id})
-    }
-
-
-    if (temp[user.id]) temp[user.id] = [...temp[user.id], obj]
-    else temp[user.id] = obj
-    usersTasks.value = [...usersTasks.value, temp]
-  } catch (e) {
-    catchErrors(e)
-  } finally {
+  }finally {
     loading.value = false
   }
 }
+
 
 const fetchUser = async () => {
   try {
@@ -143,6 +116,7 @@ const fetchUser = async () => {
 }
 
 // Composables
+const paginate = usePaginate(fetchUsers, null)
 fetchUser()
 
 </script>
