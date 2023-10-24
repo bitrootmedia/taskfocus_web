b639346f411589bba97304d047c352d4429688e4
Jakub Bernatowicz <info@bitrootmedia.com>
2023-10-24 13:04:38 +0100

    Update closed projects

--- src/components/Modals/ProjectsModal.vue
+++ src/components/Modals/ProjectsModal.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="showModal" ref="componentModalRef"
-       class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex"
-  @click.self="emit('close')">
+    class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex"
+    @click.self="emit('close')">
     <div class="relative my-6 mx-auto w-[350px]">
       <!--content-->
       <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
@@ -13,34 +13,28 @@
 
           <div>
             <span class="cursor-pointer" @click="emit('close')">
-               <i class="fas fa-window-close mr-2 text-3xl text-blueGray-400"/>
+              <i class="fas fa-window-close mr-2 text-3xl text-blueGray-400" />
             </span>
           </div>
         </div>
         <!--body-->
         <div class="relative p-6 flex-auto">
-          <Loader v-if="loading"/>
+          <Loader v-if="loading" />
 
           <div v-else class="content">
             <div class="relative mb-6">
-              <i class="fas fa-search mr-2 text-sm text-blueGray-300 absolute top-[12px] left-[8px]"/>
-              <input
-                  v-model="filter.search.value"
-                  type="text"
-                  class="border-0 pl-8 pr-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  placeholder="Search Project..."
-              />
+              <i class="fas fa-search mr-2 text-sm text-blueGray-300 absolute top-[12px] left-[8px]" />
+              <input v-model="filter.search.value" type="text"
+                class="border-0 pl-8 pr-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                placeholder="Search Project..." />
             </div>
 
             <ul v-if="projects.length">
               <li v-for="project in projects" :key="project.id" class="flex justify-between items-center gap-x-1 my-3">
                 <span class="text-lg text-blueGray-500 font-medium">{{ project?.title }}</span>
-                <button
-                    :class="{'bg-red-600': project?.id === projectId, 'bg-emerald-600': project?.id !== projectId}"
-                    class="text-white active:bg-blueGray-600 text-sm font-bold px-3 py-1 rounded shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150"
-                    type="button"
-                    @click="updateProject(project?.id === projectId ? null : project?.id)"
-                >
+                <button :class="{ 'bg-red-600': project?.id === projectId, 'bg-emerald-600': project?.id !== projectId }"
+                  class="text-white active:bg-blueGray-600 text-sm font-bold px-3 py-1 rounded shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150"
+                  type="button" @click="updateProject(project?.id === projectId ? null : project?.id)">
                   {{ project?.id === projectId ? 'Remove' : 'Select' }}
                 </button>
               </li>
@@ -53,11 +47,8 @@
             </div>
 
 
-            <Pagination
-                v-if="paginate.pagination.value.total > 1 && !loading"
-                :pagination="paginate.pagination.value"
-                v-model:query="paginate.query.value"
-            />
+            <Pagination v-if="paginate.pagination.value.total > 1 && !loading" :pagination="paginate.pagination.value"
+              v-model:query="paginate.query.value" />
           </div>
         </div>
       </div>
@@ -67,16 +58,16 @@
 </template>
 
 <script setup>
-import {catchErrors} from "../../utils";
-import {onBeforeUnmount, onMounted, ref} from "vue";
+import { catchErrors } from "../../utils";
+import { onBeforeUnmount, onMounted, ref } from "vue";
 import Loader from "./../../components/Loader/Loader.vue"
-import {useProjectStore} from "../../store/project";
-import {useCookies} from "vue3-cookies";
-import {useToast} from "vue-toastification";
-import {usePaginate} from "../../composables/usePaginate";
-import {useFilter} from "../../composables/useFilter";
+import { useProjectStore } from "../../store/project";
+import { useCookies } from "vue3-cookies";
+import { useToast } from "vue-toastification";
+import { usePaginate } from "../../composables/usePaginate";
+import { useFilter } from "../../composables/useFilter";
 import Pagination from "./../Pagination/Pagination.vue"
-import {useTasksStore} from "../../store/tasks";
+import { useTasksStore } from "../../store/tasks";
 import config from "../../config";
 
 const emit = defineEmits(['close', 'update'])
@@ -104,7 +95,7 @@ const props = defineProps({
 
 const projectStore = useProjectStore()
 const taskStore = useTasksStore()
-const {cookies} = useCookies();
+const { cookies } = useCookies();
 const toast = useToast()
 const projects = ref([])
 const componentModalRef = ref()
@@ -121,6 +112,7 @@ const fetchProjects = async () => {
       pagination: paginate.pagination.value,
       query: paginate.query.value,
       search: filter.search.value,
+      hideClosed: true
     }
     const resp = await projectStore.fetchProjects(options)
     projects.value = resp.data.results
