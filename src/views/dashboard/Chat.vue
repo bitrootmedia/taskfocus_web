f2768b792e7dc7fbf9ca57e50007badeef8906a8
Samvel <samo97danielyan@gmail.com>
2025-03-26 17:34:37 +0400

    Adding chat

---
+++ src/views/dashboard/Chat.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-container pt-6">
+
+    <div class="chat-wrapper flex gap-10">
+      <div class="left-side w-1/2 space-y-4">
+        <div class="global-search border border-gray-600 h-[50px]">Global Search</div>
+
+        <div class="flex gap-4">
+          <div class="users-list w-1/2 border border-gray-600 h-[800px]">Users List</div>
+          <div class="threads-list w-1/2 border border-gray-600 h-[800px]">Threads List</div>
+        </div>
+      </div>
+
+      <div class="right-side w-1/2 space-y-4">
+
+        <div class="chat-info border border-gray-600 h-[100px]">
+          Chat Info
+        </div>
+
+        <div class="chat-content border border-gray-600 h-[750px]">
+          <h1>Chat</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
